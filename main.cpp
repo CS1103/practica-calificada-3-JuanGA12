de270ddce1924a383cc7b4fadd5158adc3409bed
JuanGA12 <juan.galvez@utec.edu.pe>
2019-11-07 18:54:30 -0500

    segunda_entrega

--- main.cpp
+++ main.cpp
@@ -9,23 +9,31 @@
 
 using namespace std;
 int main() {
-    vector<string> nombre_oferta1;
+    vector<string> DATOS;
     string nombre_oferta;
 
     ifstream archivo("bid_example.txt");
     if(archivo.is_open()){
         while(getline(archivo,nombre_oferta)){
-            nombre_oferta1.push_back(nombre_oferta);
+            DATOS.push_back(nombre_oferta);
             cout<<nombre_oferta<<endl;
         }
     }
     archivo.close();
+    //NO SE LOGRÓ A LEER EL ARCHIVO COMO SE ESPERABA
+    //SOLO SE GUARDÓ TODOS LOS DATOS EN UN VECTOR(DATOS)
+    //CUANDO LA IDEA ERA GUARDAR LOS NOMBRES DE LOS PRODCUTOS
+    //EN UN LIST O VECTOR
+    //Y LOS ALIAS CON SUS VALORES EN MAPS
+    //YA QUE NO SE CONSIGUIÓ LA LECTURA DE ARCHIVOS
+    //SE PROCEDIÓ A "CREARLOS MANUALMENTE" EN maps.h (NO PEDIDO PERO NECESARIO PARA AL MENOS TENER ALGO)
 
     MBP();
     T_P50();
     IP11();
     S_TV_50();
 
+    cout <<endl;
     max(MACBOOK_PRO);
     max(THINKPAD_P50);
     max(IPHONE_11);
@@ -40,5 +48,14 @@ int main() {
     min(THINKPAD_P50);
     min(IPHONE_11);
     min(SAMSUNG_TV_50);
+
+    /*for(auto&i : MACBOOK_PRO){
+        auto it = find(begin(THINKPAD_P50), end(THINKPAD_P50), i.first);
+        MACBOOK_PRO.erase(it);
+    }
+
+    for(auto&i:MACBOOK_PRO){
+        cout<<i.second<<" ";
+    }*/
     return 0;
 }
